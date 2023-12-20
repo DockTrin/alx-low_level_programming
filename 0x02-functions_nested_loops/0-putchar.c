347e5bad566a0f20f955a617d0a30baee358dd4e
DockTrin <okonj5031@gmail.com>
2023-12-19 20:08:11 -0800

    A program that prints _putchar followed by a newline

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-void _putchar(char, _putchar);
+void _putchar(char _putchar);
 int main()
 {
 	_putchar("_putchar\n");
 
 	return 0;
 }
-void _putchar(char, _putchar)
+void _putchar(char _putchar)
 {}
